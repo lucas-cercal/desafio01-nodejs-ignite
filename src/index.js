951cbc03bdd98f3991248859af5a1bfee037c0b1
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-03-07 14:56:35 -0300

    Adicionando validação das rotas

--- src/index.js
+++ src/index.js
@@ -11,15 +11,23 @@ app.use(express.json())
 const users = []
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) return response.status(404).json({ error: "User not found" })
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-
   const { name, username } = request.body
+  const userExists = users.find(user => user.username === username)
 
-  if (!name) return response.status(400).json({ error: "Faltando name na requisição." })
-  if (!username) return response.status(400).json({ error: "Faltando username na requisição." })
+  if (userExists) return response.status(400).json({ error: "User already registered" })
+  if (!name) return response.status(400).json({ error: "Missing name field" })
+  if (!username) return response.status(400).json({ error: "Missing username field" })
 
   const user = {
     id: uuidv4(),
@@ -34,17 +42,32 @@ app.post('/users', (request, response) => {
 })
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const { user } = request
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
 
   return response.status(200).json(user)
 })
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { username } = request.headers
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  const user = users.find(user => user.username === username)
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
 })
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
 })
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
